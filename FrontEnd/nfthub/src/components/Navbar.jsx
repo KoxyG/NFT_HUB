ab038f43e3c0c3507f108be6fcd0ecbae6b0fa7b
Progress Ochuko Eyaadah <koxygraphics@gmail.com>
2024-03-22 02:09:53 +0100

    navbar done

---
+++ FrontEnd/nfthub/src/components/Navbar.jsx
@@ -0,0 +1,90 @@
+import { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import Image from "next/image";
+import Link from "next/link";
+import Button from "@/elements/button";
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <header
+      className="bg-dark
+         to-slate-950 sticky top-0 z-40"
+    >
+      <nav className="container mx-auto flex items-center justify-between py-4 ">
+        <div className="flex items-center cursor-pointer">
+
+          <Image width={140} height={140} className="" src="/Images/logo.png" alt="logo" />
+        
+        </div>
+
+        <div className="hidden md:flex justify-between gap-6">
+          <Link href="/">
+            <Button variant="secondary">Home</Button>
+          </Link>
+          <Link href="/explore">
+            <Button variant="secondary">Explore</Button>
+          </Link>
+          <Link href="/about">
+            <Button variant="secondary">About us</Button>
+          </Link>
+          <Link href="/create">
+            <Button variant="secondary">Create</Button>
+          </Link>
+          
+        </div>
+
+        <div className="hidden md:flex items-center md:pb-4">
+          <Button className="px-3 py-3 rounded-lg bg-gradient-to-r from-[#fe7d46] to-[#faad89]">Connect Wallet</Button>
+        </div>
+
+        <div className="md:hidden">
+          <Button className="text-white" onClick={toggleMenu}>
+            {isOpen ? "" : <FaBars className="text-2xl" />}
+          </Button>
+        </div>
+      </nav>
+
+      {isOpen && (
+        <div
+          className="fixed top-0  right-0 bottom-0 left-0 bg-dark
+         flex flex-col items-center justify-center"
+        >
+          <Button className="text-white absolute top-4 right-4" onClick={toggleMenu}>
+            <FaTimes className="text-2xl" />
+          </Button>
+
+          <Button className="px-3 py-3 rounded-lg bg-gradient-to-r from-[#fe7d46] to-[#faad89]">Connect Wallet</Button>
+          
+
+          
+          <div className="grid pt-6 gap-2 text-center">
+
+          <Link href="/">
+            <button variant="secondary">Home</button>
+          </Link>
+          <Link href="/explore">
+            <button variant="secondary">Explore</button>
+          </Link>
+          <Link href="/about">
+            <button variant="secondary">About us</button>
+          </Link>
+          <Link href="/create">
+            <button variant="secondary">Create</button>
+          </Link>
+
+          </div>
+
+
+        </div>
+      )}
+    </header>
+  );
+};
+
+export default Navbar;
---
+++ FrontEnd/nfthub/src/elements/button.jsx
@@ -0,0 +1,28 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import classNames from "classnames";
+
+const Button = ({ variant, className, children, icon, onClick, size = "md" }) => {
+  return (
+    <button
+      className={classNames(
+        {
+          "border-2 border-transparent bg-gradient-to-r from-[#fe7d46] to-[#faad89] text-white hover:text-gray-400 bg-origin-border":
+            variant === "primary",
+          "border-2 border-transparent  text-white hover:text-gray-400": variant === "secondary",
+          "border-2 text-white border-white hover:border-gray-400": variant === "outline",
+          "py-2.5 px-5": size === "md",
+          "py-1.5 px-4": size === "sm",
+        },
+        "flex flex-row gap-2 rounded-lg text-md sm:text-xl items-center bg-transparent",
+        className
+      )}
+      style={{ backgroundClip: "padding-box, border-box" }}
+      onClick={onClick}
+    >
+      {children}
+      {icon && <FontAwesomeIcon icon={icon} size="lg" />}
+    </button>
+  );
+};
+
+export default Button;
